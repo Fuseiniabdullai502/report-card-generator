987df8124a066ff6f7b901434d66601613be2038
Fuseini Abdullai <fabdullai502@gmail.com>
2025-08-16 16:52:56 +0000

    the student photo does not upload, what is the problem?

--- src/next.config.js
+++ src/next.config.js
@@ -23,24 +23,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.externals.push({
-      '@opentelemetry/instrumentation': 'commonjs @opentelemetry/instrumentation',
-      '@opentelemetry/sdk-node': 'commonjs @opentelemetry/sdk-node',
-      'dotenv': 'commonjs dotenv',
-      'express': 'commonjs express',
-      'firebase-functions': 'commonjs firebase-functions',
-      'firebase-admin': 'commonjs firebase-admin',
-      'request-ip': 'commonjs request-ip',
-      'util': 'commonjs util',
-      'handlebars': 'commonjs handlebars',
-      'best-effort-json-parser': 'commonjs best-effort-json-parser',
-    })
-    return config
-  },
 };
 
 module.exports = nextConfig;
