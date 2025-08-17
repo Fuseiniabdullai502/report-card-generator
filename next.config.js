156a5c19a3fe974065017d460ebd68aec9045801
Fuseini Abdullai <fabdullai502@gmail.com>
2025-08-17 10:47:24 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -23,29 +23,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (
-    config,
-    { isServer }
-  ) => {
-    if (isServer) {
-      config.externals.push(
-        'handlebars',
-        'express',
-        'firebase-admin',
-        'long',
-        '@opentelemetry/api',
-        '@opentelemetry/core',
-        '@opentelemetry/instrumentation',
-        '@opentelemetry/resources',
-        '@opentelemetry/sdk-trace-base',
-        '@opentelemetry/sdk-trace-node',
-        '@opentelemetry/semantic-conventions',
-        'require-in-the-middle',
-        'async_hooks'
-      );
-    }
-    return config
-  },
 };
 
 module.exports = nextConfig;
