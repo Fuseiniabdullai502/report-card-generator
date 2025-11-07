50209fe23d77a675c65e86690b4b71873b7bfde8
Fuseini Abdullai <fabdullai502@gmail.com>
2025-11-07 16:35:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.js
+++ src/next.config.js
@@ -2,33 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https' ,
-        hostname: 'upload.wikimedia.org',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+  swcMinify: true,
 };
 
 export default nextConfig;
